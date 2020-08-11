d704fb0c2f4cfd99030c1b9dd5cf1b71d0303b37
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-08-11 18:23:36 -0500

    completed tasks one through 5:

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
+const votingAge = 18;
+let age = 22;
+if (age > 18) {
+    console.log('true');
 
+}
+else {
+    console.log('false');
+}
 
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
-
-
+let varOne = 12;
+const varTwo = 24;
+if (varOne = varTwo * 0.5) {
+    varOne = varOne * 2;
+    console.log(varOne);
+}
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
-
+let numberToString = '1999';
+Number(numberToString);
+console.log(numberToString);
 
 
 
 
 //Task d: Write a function to multiply a*b 
+function multiplyNumbers(a, b) {
+    const product = (a * b);
+    return product;
+}
+const newProduct = multiplyNumbers(4, 8);
+console.log(newProduct);
 
 
 
@@ -27,8 +47,11 @@
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
-
-
+function dogYears(humanAge) {
+    let dogAge = humanAge * 7;
+    return dogAge;
+}
+console.log(dogYears(23));
 
 
 /************************************************************** Task 3 **************************************************************/
@@ -48,9 +71,35 @@
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+function dogFeeder(a, b) {
+    let age = a;
+    let weight = b;
+    let food = 0;
+    if (age > 0.167 && age < 0.333) {
+        food = weight * 0.10;
+    }
+    else if (age > 0.333 && age < 0.583) {
+        food = weight * 0.05;
+    }
+    else if (age > 0.583 && age < 1) {
+        food = weight * 0.04;
+    }
+    else if (age >= 1 && weight <= 5) {
+        food = weight * 0.05;
+    }
+    else if (age >= 1 && (weight >= 6 && weight <= 10)) {
+        food = weight * 0.04;
+    }
+    else if (age >= 1 && (weight >= 11 && weight <= 15)) {
+        food = weight * 0.03;
+    }
+    else if (age >= 1 && weight > 15) {
+        food = weight * 0.02;
+    }
+    return food;
+}
 
-
+console.log(dogFeeder(0.39, 18));
 
 
 /************************************************************** Task 4 **************************************************************/
@@ -60,20 +109,68 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+function rockPaperScissors(playerChoice) {
+    let computer = Math.random();
+    let winOrLose = 0;
+    if (playerChoice === 'rock') {
+        playerChoice = 0.33;
+        if (playerChoice === 0.33 && (computer > 0.33 && computer < 0.67)) {
+            winOrLose = 'You Lose!';
+        }
+        else if (playerChoice === computer) {
+            winOrLose = 'Draw';
+        }
+        else {
+            winOrLose = 'You Win!';
+        }
+    }
+    if (playerChoice === 'paper') {
+        playerChoice = 0.67;
+        if (playerChoice === 0.67 && (computer > 0.67 && computer < 0.99)) {
+            winOrLose = 'You Lose!';
+        }
+        else if (playerChoice === computer) {
+            winOrLose = 'Draw';
+        }
+        else {
+            winOrLose = 'You Win!';
+        }
+
+    }
+    if (playerChoice === 'scissors') {
+        playerChoice = 0.99;
+        if (playerChoice === 0.99 && (computer > 0 && computer <0.33)){
+            winOrLose = 'You Lose!';
+        }
+        else if (computer === playerChoice){
+            winOrLose = 'Draw';
+        }
+        else{
+            winOrLose = 'You Win!';
+        }
+    }
+    return winOrLose;
+}
+
+console.log(rockPaperScissors('rock'));
+
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
-
-
+function kmToMiles(kilometers) {
+    return kilometers * 0.621371;
+}
+console.log(kmToMiles(45));
 
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
-
+function feetToCm(feet) {
+    return feet * 30.48;
+}
+console.log(feetToCm(20));
 
 
 
@@ -81,7 +178,9 @@
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+for (i = 99; i === 0; i--) {
+
+}
 
 
 
@@ -94,10 +193,10 @@
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
-  
 
-  
-  
+
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
